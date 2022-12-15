acfd0af37e43e96456c6132ce21ec94868277270
ThiagoMorenoCordeiro <cordeiro.thiago@hotmail.com>
2022-12-15 17:47:52 -0300

    commit 0006

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function fazGet(url){
 }
 
 function criaLinha(bcdata){
-
+    console.log(bcdata)
     linha = document.createElement("tr")
     tdData = document.createElement("td")
     tdValor = document.createElement("td")
